673e74235251ccd8d556e3adec9be51ab912a07f
Kamil <kamil@DESKTOP-VJGHBUA.localdomain>
2023-08-06 15:18:00 +0200

    prepare dist

--- assets/LimitedWidthContainer-5c47549d.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,a as n,c as _,h as s}from"./index-91e290ed.js";const a={},o={class:"max-width md:px-11 px-6"};function d(e,c){return n(),_("div",o,[s(e.$slots,"default",{},void 0,!0)])}const r=t(a,[["render",d],["__scopeId","data-v-03f7a146"]]);export{r as L};
